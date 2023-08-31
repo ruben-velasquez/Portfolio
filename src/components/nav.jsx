c0254ee477ed45050389b9561b4232ae5f791639
Rubén Velásquez <73437475+ruben-velasquez@users.noreply.github.com>
2023-08-31 20:04:33 +0000

    Ahora la barra de navegación se queda pegada arriba del todo

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -9,7 +9,7 @@ export default function Nav() {
   const logoImage = "https://avatars.githubusercontent.com/u/73437475?v=4";
 
   return (
-    <nav className="h-[90px] flex flex-row py-5 px-10 sm:px-[50px] md:px-5 lg:px-[200px] justify-between">
+    <nav className="h-[90px] flex flex-row py-5 px-10 sm:px-[50px] md:px-5 lg:px-[200px] justify-between fixed top-0 left-0 right-0">
       <div><h1 className="font-bold">Logo</h1></div>
       <ul className="lg:flex flex-row gap-3 hidden">
         <li>
