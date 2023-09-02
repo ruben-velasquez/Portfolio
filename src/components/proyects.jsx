e718df3c9db38156f14566adeeaa2c41bddf8d37
Rubén Velásquez <73437475+ruben-velasquez@users.noreply.github.com>
2023-09-02 15:15:21 +0000

    Resaltado título de la sección de Proyectos

--- src/components/proyects.jsx
+++ src/components/proyects.jsx
@@ -16,7 +16,7 @@ export default async function Proyects() {
       className="px-10 sm:px-[50px] md:px-5 lg:px-[200px] flex flex-col gap-4"
     >
       <h1
-        className={`w-full text-3xl font-bold text-center ${kanit.className}`}
+        className={`w-full text-3xl font-bold text-center text-highlight ${kanit.className}`}
       >
         Proyects
       </h1>
