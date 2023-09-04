f8658b8a724fef0609a09618761af4cf816c0165
ruben-velasquez <rualvemo@hotmail.com>
2023-09-04 20:50:59 -0300

    Hecha componente de cliente a la barra de navegación

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { Kanit } from "next/font/google";
 
@@ -7,7 +8,7 @@ export default function Nav() {
   return (
     <nav className="z-40 bg-navigation items-center h-[70px] flex flex-row py-5 px-10 sm:px-[50px] md:px-5 lg:px-[200px] justify-between fixed top-0 left-0 right-0">
       <div>
-        <h1 className={`font-bold text-2xl ${kanit.className} text-highlight`}>
+        <h1 className={`${kanit.className} font-bold text-2xl text-highlight`}>
           Logo
         </h1>
       </div>
