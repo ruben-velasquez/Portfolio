3b8a479569e8785b1bc865120cb0217d38bc83f1
ruben-velasquez <rualvemo@hotmail.com>
2023-09-07 19:23:52 -0300

    Se añadió un botón para refrescar los proyectos en el dashboard

--- components/projectsDashboard.jsx
+++ components/projectsDashboard.jsx
@@ -3,19 +3,42 @@ import ProjectsCard from "./projectCard";
 import AddProjectsCard from "@/components/addProjectCard";
 import { Kanit } from "next/font/google";
 import { Divider } from "@nextui-org/react";
+import { MdReplay } from "react-icons/md";
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 const kanit = Kanit({ subsets: ["latin"], weight: "400" });
 
 export default function ProjectsDashboard({ projects }) {
+  const router = useRouter();
+
   return (
     <>
-      <div className="space-y-1 text-left w-full">
-        <h4 className={`text-lg font-medium text-highlight ${kanit.className}`}>
-          Projects
-        </h4>
-        <p className="text-small text-default-400">
-          All the projects made by me.
-        </p>
+      <div className={"flex justify-between items-center w-full"}>
+        <div className="space-y-1 text-left">
+          <h4
+            className={`text-lg font-medium text-highlight ${kanit.className}`}
+          >
+            Projects
+          </h4>
+          <p className="text-small text-default-400">
+            All the projects made by me.
+          </p>
+        </div>
+        <Button
+          isIconOnly
+          size="md"
+          color="primary"
+          variant={"bordered"}
+          aria-label="Like"
+          className={"group hover:bg-action"}
+          onPress={router.refresh}
+        >
+          <MdReplay
+            size={16}
+            className="text-action transition-all duration-700 group-hover:text-slate-200"
+          />
+        </Button>
       </div>
       <Divider className="my-4" />
 
